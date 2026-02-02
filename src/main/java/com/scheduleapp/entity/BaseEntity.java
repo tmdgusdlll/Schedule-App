939f6a3d4cce5cd436bb8844bf3f3ea7d3f2ee4c
tmdgsudl <leesh27955@gmail.com>
2026-02-02 21:04:48 +0900

    [Lv.1] 기본 Entity 생성

---
+++ src/main/java/com/scheduleapp/entity/BaseEntity.java
@@ -0,0 +1,4 @@
+package com.scheduleapp.entity;
+
+public class BaseEntity {
+}
---
+++ src/main/java/com/scheduleapp/entity/Schedule.java
@@ -0,0 +1,24 @@
+package com.scheduleapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "schedules")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String contents;
+    private String name;
+    private int pw;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
