b9790108ba5d99647aaa5fbd9418ff8966efd2ce
tmdgsudl <leesh27955@gmail.com>
2026-02-03 10:47:14 +0900

    [Lv.1] Entity 클래스 생성자, update를 위한 메서드 미리 구현

--- src/main/java/com/scheduleapp/entity/Schedule.java
+++ src/main/java/com/scheduleapp/entity/Schedule.java
@@ -13,12 +13,28 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Schedule extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 고유 식별자 ID 생성
     private Long id;
     private String title;
     private String contents;
     private String name;
     private int pw;
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt; // 작성, 수정일 (JpaAuditing 활용)
     private LocalDateTime modifiedAt;
+
+    public Schedule(String contents, String title, String name, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.contents = contents;
+        this.title = title;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public void update(String contents, String title, String name, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.contents = contents;
+        this.title = title;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
 }
