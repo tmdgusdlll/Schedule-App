3f8ff686f2ecc6ab821cf119e38d8c7d6e6af605
tmdgsudl <leesh27955@gmail.com>
2026-02-04 12:27:03 +0900

    [Lv.7] 글자 제한, 필수값 처리

--- src/main/java/com/scheduleapp/entity/Comment.java
+++ src/main/java/com/scheduleapp/entity/Comment.java
@@ -13,8 +13,11 @@ public class Comment extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100, nullable = false) // Lv.7 댓글 내용 100자 제한, 필수값 처리
     private String contents;
+    @Column(nullable = false) // Lv.7 작성자명 필수값 처리
     private String name;
+    @Column(nullable = false) // Lv.7 비밀번호 필수값 처리
     private String password;
 //    // Schedule 클래스와 연결관계를 갖기위한 추가적인 어노테이션과 필드, 생성자까지
 //    @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/scheduleapp/entity/Schedule.java
+++ src/main/java/com/scheduleapp/entity/Schedule.java
@@ -13,9 +13,13 @@ public class Schedule extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 고유 식별자 ID 생성
     private Long id;
+    @Column(length = 30, nullable = false) // Lv.7 일정 제목 30자 제한, 필수값처리
     private String title;
+    @Column(length = 200, nullable = false) // Lv.7 일정 내용 200자 제한, 필수값처리
     private String contents;
+    @Column(nullable = false) // Lv.7 작성자명 필수값 처리
     private String name;
+    @Column(nullable = false) // Lv.7 비밀번호 필수값 처리
     private String password;
 
     public Schedule(String title, String contents, String name, String password) {
